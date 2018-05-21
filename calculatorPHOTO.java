48d568771baef965d5ccf3d1858efe4c1b338d38
106021055 <32056108+106021055@users.noreply.github.com>
2018-05-21 15:31:52 +0800

    Add files via upload

---
+++ calculatorPHOTO.java
@@ -0,0 +1,216 @@
+package NEWTwo;
+
+        import javax.swing.*;
+        import java.awt.*;
+        import java.awt.event.ActionEvent;
+        import java.awt.event.ActionListener;
+
+class calculatorPHOTO extends JFrame{
+    public JLabel label = new JLabel("0");
+    public JPanel btnpanel = new JPanel(new GridLayout(4, 5, 4, 4));
+    public JButton buttons[] = new JButton[20];
+    public ImageIcon images[] = new ImageIcon[10];
+    public double v1 = 0;
+    public double v2 = 0;
+    public int op = -1;
+    public boolean bool = true;
+
+    public calculatorPHOTO(){
+        tt();
+    }
+
+    public void tt(){
+        setSize(400, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        label.setFont(new Font(null, Font.BOLD, 48));
+        label.setHorizontalAlignment(SwingConstants.RIGHT);
+        add(label, BorderLayout.NORTH);
+        add(btnpanel, BorderLayout.CENTER);
+        for (int i = 0; i < 10; i ++){
+            images[i] = new ImageIcon(i + ".PNG");
+        }
+
+        for (int i = 0; i < 20; i++){
+            switch (i){
+                case 0:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[7]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 1:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[8]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 2:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[9]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 3:
+                    buttons[i] = new JButton("/");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 4:
+                    buttons[i] = new JButton("C");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 5:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[4]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 6:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[5]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 7:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[6]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 8:
+                    buttons[i] = new JButton("*");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 9:
+                    buttons[i] = new JButton("sqrt");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 10:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[1]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 11:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[2]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 12:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[3]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 13:
+                    buttons[i] = new JButton("-");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 14:
+                    buttons[i] = new JButton("PI");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 15:
+                    buttons[i] = new JButton();
+                    buttons[i].setIcon(images[0]);
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 16:
+                    buttons[i] = new JButton(".");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 17:
+                    buttons[i] = new JButton("=");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 18:
+                    buttons[i] = new JButton("+");
+                    btnpanel.add(buttons[i]);
+                    break;
+                case 19:
+                    buttons[i] = new JButton("EXIT");
+                    btnpanel.add(buttons[i]);
+                    break;
+            }
+
+            buttons[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JButton tembtn = (JButton)e.getSource();
+                    switch (tembtn.getText()){
+                        case "C":
+                            v1 = 0;
+                            v2 = 0;
+                            op = -1;
+                            label.setText("0");
+                            break;
+                        case "sqrt":
+                            label.setText(Double.toString(Math.sqrt(Double.parseDouble(label.getText()))));
+                            break;
+                        case "PI":
+                            label.setText(Double.toString(Math.PI));
+                            break;
+                        case "EXIT":
+                            System.exit(0);
+                            break;
+                        case "+":
+                            v1 = Double.parseDouble(label.getText());
+                            op = 0;
+                            bool = false;
+                            break;
+                        case "-":
+                            v1 = Double.parseDouble(label.getText());
+                            op = 1;
+                            bool = false;
+                            break;
+                        case "*":
+                            v1 = Double.parseDouble(label.getText());
+                            op = 2;
+                            bool = false;
+                            break;
+                        case "/":
+                            v1 = Double.parseDouble(label.getText());
+                            op = 3;
+                            bool = false;
+                            break;
+                        case "=":
+                            v2 = Double.parseDouble(label.getText());
+                            double v3 = 0;
+                            switch (op){
+                                case 0:
+                                    v3 = v1 + v2;
+                                    break;
+                                case 1:
+                                    v3 = v1 - v2;
+                                    break;
+                                case 2:
+                                    v3 = v1 * v2;
+                                    break;
+                                case 3:
+                                    v3 = v1 / v2;
+                                    break;
+                            }
+                            if (op > -1){
+                                label.setText(Double.toString(v3));
+                            }
+                            op = -1;
+                            label.setText(Double.toString(v3));
+                            break;
+                        case ".":
+                            if (!label.getText().matches("-*\\d+\\.\\d*")){
+                                label.setText(label.getText() + ".");
+                            }
+                            break;
+                        default:
+                            if (label.getText().equals("0") || (op > -1 && !bool)){
+                                for (int i = 0; i < 10; i++){
+                                    if (tembtn.getIcon().equals(images[i])){
+                                        label.setText(Integer.toString(i));
+                                    }
+                                }
+                                bool = true;
+                            }else {
+                                for (int i = 0; i < 10; i++){
+                                    if (tembtn.getIcon().equals(images[i])){
+                                        label.setText(label.getText() + i);
+                                    }
+                                }
+                            }
+                    }
+                }
+            });
+        }
+    }
+}
