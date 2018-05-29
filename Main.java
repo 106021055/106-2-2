21675566f51133d054df4f16b2aa5239ebd98a65
106021055 <32056108+106021055@users.noreply.github.com>
2018-05-29 12:52:33 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,8 @@
-package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-            MainFrame abc = new MainFrame();
-            abc.setVisible(true);
+        Taco a = new Taco();
+
+        a.setVisible(true);
     }
 }
